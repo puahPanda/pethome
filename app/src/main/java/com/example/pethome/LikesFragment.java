31508ce89ab3a497d8488d9ac1e59f04246b381d
Hsin Kai <45195644+Hsinkai2000@users.noreply.github.com>
2023-09-15 02:08:09 +0800

    migrated liked page to fragment and implemented recyclerView

--- app/src/main/java/com/example/pethome/LikesFragment.java
+++ app/src/main/java/com/example/pethome/LikesFragment.java
@@ -3,6 +3,9 @@ package com.example.pethome;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,51 +17,23 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class LikesFragment extends Fragment {
+    RecyclerView recyclerView;
+    RecyclerView.Adapter likedPetsAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    String [] names = {"Joshua", "Zhim", "Lino", "Rach", "Cherry", "Ban", "Lino", "Rach"};
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LikesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LikesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LikesFragment newInstance(String param1, String param2) {
-        LikesFragment fragment = new LikesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    String [] age = {"6M", "1Y", "2M", "20D", "5Y", "1Y6M","2M", "20D"};
+    int [] images = {R.drawable.cat1, R.drawable.duck1, R.drawable.gecko, R.drawable.babydog, R.drawable.gecko, R.drawable.cat1, R.drawable.duck1, R.drawable.gecko};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_likes, container, false);
+        View view = inflater.inflate(R.layout.fragment_likes, container, false);
+        recyclerView = view.findViewById(R.id.likesRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new GridLayoutManager(getContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        likedPetsAdapter = new LikedPetsAdapter(getContext(), names,age,images);
+        recyclerView.setAdapter(likedPetsAdapter);
+        return view;
     }
 }
