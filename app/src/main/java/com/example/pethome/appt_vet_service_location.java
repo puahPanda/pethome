bb5e8061552bfc049013e5f94c7a53fdd54814ff
mingzhi987 <106290967+mingzhi987@users.noreply.github.com>
2023-09-16 22:23:26 +0800

    first commit

---
+++ app/src/main/java/com/example/pethome/appt_vet_service_location.java
@@ -0,0 +1,14 @@
+package com.example.pethome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class appt_vet_service_location extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appt_vet_service_location);
+    }
+}
