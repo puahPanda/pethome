54babc997df46ff87a2773cd2dc42191df1b6084
mingzhi987 <106290967+mingzhi987@users.noreply.github.com>
2023-09-16 23:08:08 +0800

    first commit

--- app/src/main/java/com/example/pethome/ApptVetServiceLocation.java
+++ app/src/main/java/com/example/pethome/ApptVetServiceLocation.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class appt_vet_service_location extends AppCompatActivity {
+public class ApptVetServiceLocation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
