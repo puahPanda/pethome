220d3d1169f77f2d299ae1d8c5270b02564adeda
lqt1984 <QUANGTUA001@e.ntu.edu.sg>
2023-10-01 01:22:18 +0800

    filter_page updated

--- app/src/main/java/com/example/pethome/FilterPage.java
+++ app/src/main/java/com/example/pethome/FilterPage.java
@@ -3,12 +3,33 @@ package com.example.pethome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class FilterPage extends AppCompatActivity {
+    private Button btn_1;
+    private TextView seekBar_text;
+    private SeekBar seekBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_page);
+
+        seekBar_text = findViewById(R.id.seekbar_text);
+        seekBar = findViewById(R.id.seekbar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                seekBar_text.setText(i + " months");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 }
