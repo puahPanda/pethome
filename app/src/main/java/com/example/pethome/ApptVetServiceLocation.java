242ddba408be67e4930a25aca7879db12cb84430
mingzhi987 <106290967+mingzhi987@users.noreply.github.com>
2023-10-31 14:23:51 +0800

    minor adjustments
    Update activity_appt_vet_service_location.xml
    
    Update ProfileFragment.java

--- app/src/main/java/com/example/pethome/ApptVetServiceLocation.java
+++ app/src/main/java/com/example/pethome/ApptVetServiceLocation.java
@@ -1,14 +1,36 @@
 package com.example.pethome;
 
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class ApptVetServiceLocation extends AppCompatActivity {
+import androidx.fragment.app.Fragment;
+
+public class ApptVetServiceLocation extends Fragment {
+    Button btn_next;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appt_vet_service_location);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.activity_appt_vet_service_location, container, false);
+
+
+        btn_next = view.findViewById(R.id.btn_next);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getContext(), make_appt_pet.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 }
