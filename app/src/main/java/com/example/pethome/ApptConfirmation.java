8d749a2c65df88083aaa327b8b274e9e12a44938
mingzhi987 <106290967+mingzhi987@users.noreply.github.com>
2023-10-31 17:07:54 +0800

    Update ApptConfirmation.java

--- app/src/main/java/com/example/pethome/ApptConfirmation.java
+++ app/src/main/java/com/example/pethome/ApptConfirmation.java
@@ -1,5 +1,6 @@
 package com.example.pethome;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,8 @@ public class ApptConfirmation extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Button btn_home;
 
     public ApptConfirmation() {
         // Required empty public constructor
@@ -59,6 +63,16 @@ public class ApptConfirmation extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_appt_confirmation, container, false);
+        View view = inflater.inflate(R.layout.fragment_appt_confirmation, container, false);
+
+        btn_home = view.findViewById(R.id.btnDismiss);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getContext(), ProfileFragment.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 }
