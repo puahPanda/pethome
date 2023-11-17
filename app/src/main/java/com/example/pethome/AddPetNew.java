e306eb97f401cf576c6eaaef036bfe365dc0ae3a
Hsin Kai <hsinkai2000@gmail.com>
2023-11-17 22:54:04 +0800

    AddPetNew.java

--- app/src/main/java/com/example/pethome/AddPetNew.java
+++ app/src/main/java/com/example/pethome/AddPetNew.java
@@ -3,6 +3,8 @@ package com.example.pethome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddPetNew extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class AddPetNew extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pet_new);
+
+        populateSpinner();
+
+    }
+
+    private void populateSpinner() {
+        Spinner genderSpinner = findViewById(R.id.genderSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/pethome/MyProfileNewFragment.java
+++ app/src/main/java/com/example/pethome/MyProfileNewFragment.java
@@ -10,10 +10,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.example.pethome.R;
 import com.example.pethome.make_appt_pet;
 
 public class MyProfileNewFragment extends Fragment {
+    Button btnEditProfile;
 
     public MyProfileNewFragment() {
 
@@ -24,6 +27,15 @@ public class MyProfileNewFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_my_profile_new, container, false);
+        btnEditProfile = view.findViewById(R.id.btnEditProfile);
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), AddPetNew.class);
+                startActivity(i);
+            }
+        });
 
         return view;
     }
